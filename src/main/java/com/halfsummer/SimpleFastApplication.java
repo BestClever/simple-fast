2fd31b39bcc4c5e6d7a1389e3a2d7d8512ebbf83
xiacong <xiacong@talkweb.com.cn>
2020-11-17 09:42:02 +0800

    Initial commit

---
+++ src/main/java/com/halfsummer/SimpleFastApplication.java
@@ -0,0 +1,13 @@
+package com.halfsummer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halfsummer/simplefast/SimpleFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halfsummer.simplefast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
